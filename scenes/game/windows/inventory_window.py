6eff3c22f7fb76cee803d138349dd33debaf0692
nethys <arzhul@gmail.com>
2017-08-14 08:10:02 -0400

    Fixed inventory layering and callback issues.

--- scenes/game/windows/inventory_window.py
+++ scenes/game/windows/inventory_window.py
@@ -1,50 +1,58 @@
-from clubsandwich.ui import UIScene, WindowView, LayoutOptions, KeyAssignedListView, ButtonView, Scene
+from functools import partial
+
+from clubsandwich.ui import UIScene, WindowView, LayoutOptions, KeyAssignedListView, ButtonView
 
 
 class InventoryWindow(UIScene):
     def __init__(self, wielded_items, worn_items, inventory_items):
-        wielded_button_generator = (
-            ButtonView(item.name, lambda: self.describe_item(item))
+        self.covers_screen = True
+        wielded_buttons = [
+            ButtonView(item.name, callback=partial(self.describe_item, item))
             for item in wielded_items
-        )
-        worn_button_generator = (
-            ButtonView(item.name, lambda: self.describe_item(item))
+        ]
+        worn_buttons = [
+            ButtonView(item.name, callback=partial(self.describe_item, item))
             for item in worn_items
-        )
-        inventory_button_generator = (
-            ButtonView(item.name, lambda: self.describe_item(item))
+        ]
+        inventory_buttons = [
+            ButtonView(item.name, callback=partial(self.describe_item, item))
             for item in inventory_items
+        ]
+
+        window_view = WindowView(
+            "Items",
+            subviews=[
+                KeyAssignedListView(
+                    wielded_buttons,
+                    value_column_width=16,
+                    layout_options=LayoutOptions(left=0.1, width=0.3, height=0.3, top=0, right=None, bottom=None)
+                ),
+                KeyAssignedListView(
+                        worn_buttons,
+                        value_column_width=16,
+                        layout_options=LayoutOptions(left=0.1, width=0.3, height=0.3, top=0.3, right=None, bottom=None)
+                ),
+                KeyAssignedListView(
+                        inventory_buttons,
+                        value_column_width=16,
+                        layout_options=LayoutOptions(left=0.1, width=0.3, height=0.3, top=0.6, right=None, bottom=None)
+                )
+            ]
         )
-        wielded_view = WindowView(
-            'Wielded Items',
-            layout_options=LayoutOptions.centered(60, 20),
-            subviews=[KeyAssignedListView(wielded_button_generator)]
-        )
-        worn_view = WindowView(
-            'Worn Items',
-            layout_options=LayoutOptions.centered(60, 20),
-            subviews=[KeyAssignedListView(worn_button_generator)]
-        )
-        inventory_view = WindowView(
-            'Inventory',
-            layout_options=LayoutOptions.centered(60, 20),
-            subviews=[KeyAssignedListView(inventory_button_generator)]
-        )
-        super().__init__([wielded_view, worn_view, inventory_view])
+        super().__init__(window_view)
 
     def describe_item(self, item):
         self.director.push_scene(ItemDetailWindow(item))
 
 
-class ItemDetailWindow(WindowView):
+class ItemDetailWindow(UIScene):
     def __init__(self, chosen_item):
         self.covers_screen = False
         view = WindowView(
-            chosen_item.name
+            chosen_item.name,
+            layout_options=LayoutOptions(left=None, width=0.3, height=0.7, top=0.05, right=0.25, bottom=None)
         )
-        super().__init__()
+        super().__init__(view)
 
     def terminal_read(self, val):
         self.director.pop_scene()
-
-
